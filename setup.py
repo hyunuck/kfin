e2310b706f666e9127edecab00a35d78ada8c5b8
hyunuck <gusdnr@gmail.com>
2023-02-17 17:02:26 +0900

    add PyPI information

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup, find_packages
 
-setup(name='Eu4ng-test',  # 패키지 명
-      version='0.1.0',
-      description='Open API wrapper library for Korean financial organizations',
+setup(name='kfin',
+      version='0.0.1',
+      description='Open API wrapper library for Korea financial organizations',
       author='Hyunuck Kim',
       author_email='gusdnr@gmail.com',
-      url='https://github.com/hyunuck',
-      license='BSD License',
+      url='https://github.com/hyunuck/kfin',
+      license='MIT License',
       python_requires='>=3',
+      packages=find_packages(exclude=['docs', 'tests*']),
       install_requires=[
           "httpx==^0.23.3"
       ],
